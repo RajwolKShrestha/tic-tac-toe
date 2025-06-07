d27052731aa107ee464e57d4fa5eda294ab3b9b9
RajwolKShrestha <rajwolrks2005@gmail.com>
2025-06-07 20:04:06 +0545

    player v/s computer starter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<windows.h>
-#include<ctime>
+#include <iostream>
+#include <windows.h>
+#include <ctime>
 
 using namespace std;
 
@@ -15,23 +15,23 @@ int check_draw(char *arr);
 void block_display(char *arr)
 {
     system("cls");
-    cout<<"       |"<<"       |"<<"       "<<endl;
-    cout<<"   "<<arr[0]<<"   |"<<"   "<<arr[1]<<"   |"<<"   "<<arr[2]<<"   "<<endl;
-    cout<<"_______|"<<"_______|"<<"_______"<<endl;
-    cout<<"       |"<<"       |"<<"       "<<endl;
-    cout<<"   "<<arr[3]<<"   |"<<"   "<<arr[4]<<"   |"<<"   "<<arr[5]<<"   "<<endl;
-    cout<<"_______|"<<"_______|"<<"_______"<<endl;
-    cout<<"       |"<<"       |"<<"       "<<endl;
-    cout<<"   "<<arr[6]<<"   |"<<"   "<<arr[7]<<"   |"<<"   "<<arr[8]<<"   "<<endl;
-    cout<<"       |"<<"       |"<<"       "<<endl;
+    cout << "       |" << "       |" << "       " << endl;
+    cout << "   " << arr[0] << "   |" << "   " << arr[1] << "   |" << "   " << arr[2] << "   " << endl;
+    cout << "_______|" << "_______|" << "_______" << endl;
+    cout << "       |" << "       |" << "       " << endl;
+    cout << "   " << arr[3] << "   |" << "   " << arr[4] << "   |" << "   " << arr[5] << "   " << endl;
+    cout << "_______|" << "_______|" << "_______" << endl;
+    cout << "       |" << "       |" << "       " << endl;
+    cout << "   " << arr[6] << "   |" << "   " << arr[7] << "   |" << "   " << arr[8] << "   " << endl;
+    cout << "       |" << "       |" << "       " << endl;
 }
 
 int check_winner(char *arr, char symbol)
 {
     int winPatterns[8][3] = {
-        {0,1,2}, {3,4,5}, {6,7,8},
-        {0,3,6}, {1,4,7}, {2,5,8},
-        {0,4,8}, {2,4,6}
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+        {0, 4, 8}, {2, 4, 6}
     };
 
     for (int i = 0; i < 8; ++i)
@@ -45,11 +45,11 @@ int check_winner(char *arr, char symbol)
 
 int check_draw(char *arr)
 {
-    for(int i = 0; i < 9; i++)
-        if(arr[i] == ' ')
+    for (int i = 0; i < 9; i++)
+        if (arr[i] == ' ')
             return 0;
 
-    if(check_winner(arr, 'X') == 0 && check_winner(arr, 'O') == 0)
+    if (check_winner(arr, 'X') == 0 && check_winner(arr, 'O') == 0)
         return 1;
 
     return 0;
@@ -62,11 +62,10 @@ int human_player(char *arr)
         block_display(arr);
         cout << "Enter a position (1-9): ";
         cin >> n;
-        if (n >= 1 && n <= 9 && arr[n-1] == ' ') {
-            arr[n-1] = 'X';
+        if (n >= 1 && n <= 9 && arr[n - 1] == ' ') {
+            arr[n - 1] = 'X';
             break;
-        } 
-        else {
+        } else {
             cout << "Invalid move. Try again." << endl;
             system("pause");
         }
@@ -111,24 +110,48 @@ int computer_player(char *arr)
 
 int main()
 {
-    srand(time(0));  // Seeding randomness once
+    srand(time(0));  // Seed the RNG
 
     char choice;
     do {
-        char place[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        char place[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        int starter;
         bool play = false;
 
+        do {
+            system("cls");
+            cout << "Welcome to TIC-TAC-TOE GAME!!" << endl;
+            cout << "Please Enter your choice:\n";
+            cout << "1. Player begins first.\n";
+            cout << "2. Computer begins first.\n";
+            cin >> starter;
+
+            if (starter == 1 || starter == 2)
+                break;
+
+            cout << "Invalid choice. Try again.\n";
+            system("pause");
+
+        } while (true);
+
+        // Start the game
         while (true) {
-            play = human_player(place);
-            if (play)
-                break;
-            play = computer_player(place);
-            if (play)
-                break;
+            if (starter == 1) {
+                play = human_player(place);
+                if (play) break;
+                play = computer_player(place);
+                if (play) break;
+            } else {
+                play = computer_player(place);
+                if (play) break;
+                play = human_player(place);
+                if (play) break;
+            }
         }
 
-        cout << "Play another game? (Y/N): ";
+        cout << "\nPlay another game? (Y/N): ";
         cin >> choice;
+
     } while (choice == 'Y' || choice == 'y');
 
     cout << "Thanks for playing!" << endl;
